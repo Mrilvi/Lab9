329ffc1a2d82e538a3a8f5dd1157e38eeb88380a
Maria Ilieva <maria.ilieva0609@gmail.com>
2024-04-12 08:18:07 -0400

    Maria Ilieva Commit

---
+++ MariaLab9.py
@@ -0,0 +1,13 @@
+'''
+
+This is Maria Ilieva's main and encode functions file
+
+'''
+
+def encode(password):
+    pass
+
+if __name__ == '__main__':
+    while True:
+        # create menu
+        break
